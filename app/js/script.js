11887cddac4681f9db7f78561ae41ce2cd66c280
FANTAZER <kuznecovs45@gmail.com>
2018-08-06 20:20:18 +0300

    Initial commit

---
+++ app/js/script.js
@@ -0,0 +1,106 @@
+
+$(document).ready(function(){
+	$(function(){
+	    $('.wrap').mixItUp();
+	});
+
+
+		$('.b-filter-head__el').click(function(){
+		var currentTab = $(this).index();
+			$('.b-filter-head__el').removeClass('b-filter-head__el--active');
+			$(this).addClass('b-filter-head__el--active');
+			$('.b-wrap').each(function(){
+				if($(this).index()==currentTab){
+					$(this).addClass('b-wrap--active')
+				}else{
+					$(this).removeClass('b-wrap--active')
+				}
+			})
+		});
+
+
+	//for init SVG 
+	svg4everybody();
+	// ==== clear storage =====
+	 localStorage.clear();
+	 sessionStorage.clear();
+	 $(window).unload(function(){
+		 localStorage.clear();
+	 });
+	// ==== clear storage end =====
+
+	
+	/* ###### For SlideToggle Elements  ######*/
+	/*var hideToggle = function(targetClick,toggleEl) {
+		$(targetClick).click(function(event){
+				event.stopPropagation();
+				$(toggleEl).slideToggle("fast");
+		});
+		$(toggleEl).on("click", function (event) {
+			event.stopPropagation();
+		});
+		$(document).on("click", function () {
+				$(toggleEl).hide();
+		});
+	}
+	hideToggle('.icon-bars','.top-menu_link');*/
+
+})
+
+//cash SVG
+
+;( function( window, document )
+{
+	'use strict';
+
+	var file  = 'img/pack.html',
+		revision = 1;
+
+	if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
+		return true;
+
+	var isLocalStorage = 'localStorage' in window && window[ 'localStorage' ] !== null,
+		request,
+		data,
+		insertIT = function()
+		{
+			document.body.insertAdjacentHTML( 'afterbegin', data );
+		},
+		insert = function()
+		{
+			if( document.body ) insertIT();
+			else document.addEventListener( 'DOMContentLoaded', insertIT );
+		};
+
+	if( isLocalStorage && localStorage.getItem( 'inlineSVGrev' ) == revision )
+	{
+		data = localStorage.getItem( 'inlineSVGdata' );
+		if( data )
+		{
+			insert();
+			return true;
+		}
+	}
+
+	try
+	{
+		request = new XMLHttpRequest();
+		request.open( 'GET', file, true );
+		request.onload = function()
+		{
+			if( request.status >= 200 && request.status < 400 )
+			{
+				data = request.responseText;
+				insert();
+				if( isLocalStorage )
+				{
+					localStorage.setItem( 'inlineSVGdata',  data );
+					localStorage.setItem( 'inlineSVGrev',   revision );
+				}
+			}
+		}
+		request.send();
+	}
+	catch( e ){}
+
+}( window, document ) );
---
+++ dist/js/script.js
@@ -0,0 +1,106 @@
+
+$(document).ready(function(){
+	$(function(){
+	    $('.wrap').mixItUp();
+	});
+
+
+		$('.filter-head__el').click(function(){
+		var currentTab = $(this).index();
+			$('.filter-head__el').removeClass('filter-head__el--active');
+			$(this).addClass('filter-head__el--active');
+			$('.wrap').each(function(){
+				if($(this).index()==currentTab){
+					$(this).addClass('wrap--active')
+				}else{
+					$(this).removeClass('wrap--active')
+				}
+			})
+		});
+
+
+	//for init SVG 
+	svg4everybody();
+	// ==== clear storage =====
+	 localStorage.clear();
+	 sessionStorage.clear();
+	 $(window).unload(function(){
+		 localStorage.clear();
+	 });
+	// ==== clear storage end =====
+
+	
+	/* ###### For SlideToggle Elements  ######*/
+	/*var hideToggle = function(targetClick,toggleEl) {
+		$(targetClick).click(function(event){
+				event.stopPropagation();
+				$(toggleEl).slideToggle("fast");
+		});
+		$(toggleEl).on("click", function (event) {
+			event.stopPropagation();
+		});
+		$(document).on("click", function () {
+				$(toggleEl).hide();
+		});
+	}
+	hideToggle('.icon-bars','.top-menu_link');*/
+
+})
+
+//cash SVG
+
+;( function( window, document )
+{
+	'use strict';
+
+	var file  = 'img/pack.html',
+		revision = 1;
+
+	if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
+		return true;
+
+	var isLocalStorage = 'localStorage' in window && window[ 'localStorage' ] !== null,
+		request,
+		data,
+		insertIT = function()
+		{
+			document.body.insertAdjacentHTML( 'afterbegin', data );
+		},
+		insert = function()
+		{
+			if( document.body ) insertIT();
+			else document.addEventListener( 'DOMContentLoaded', insertIT );
+		};
+
+	if( isLocalStorage && localStorage.getItem( 'inlineSVGrev' ) == revision )
+	{
+		data = localStorage.getItem( 'inlineSVGdata' );
+		if( data )
+		{
+			insert();
+			return true;
+		}
+	}
+
+	try
+	{
+		request = new XMLHttpRequest();
+		request.open( 'GET', file, true );
+		request.onload = function()
+		{
+			if( request.status >= 200 && request.status < 400 )
+			{
+				data = request.responseText;
+				insert();
+				if( isLocalStorage )
+				{
+					localStorage.setItem( 'inlineSVGdata',  data );
+					localStorage.setItem( 'inlineSVGrev',   revision );
+				}
+			}
+		}
+		request.send();
+	}
+	catch( e ){}
+
+}( window, document ) );
